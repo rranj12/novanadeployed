c10c50523cdde654d9e92a92ce8957a2654e0b2b
rranj12 <rishabh12ranjan@gmail.com>
2025-07-05 18:09:04 -0400

    Changed output for config to server to try fix to images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,4 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({output: 'static', adapter: vercel({imageService: true,})});
+export default defineConfig({output: 'server', adapter: vercel({imageService: true,})});
